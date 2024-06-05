756f32e507aec68230d3538418e34b0557005fd6
midorinom <107839253+midorinom@users.noreply.github.com>
2024-06-06 01:51:16 +0800

    set defaultChecked for SelectRegion

--- app/ui/home/first-timer/FirstTimer.tsx
+++ app/ui/home/first-timer/FirstTimer.tsx
@@ -19,6 +19,7 @@ export default function FirstTimer({ classes }: FirstTimerProps) {
             onChange={() => {
               setCharacterChecked(false);
             }}
+            defaultChecked
           />
           <div
             className={
