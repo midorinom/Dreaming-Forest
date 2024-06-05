aa92ea626311b16264fb7ec657dd38137558844f
midorinom <107839253+midorinom@users.noreply.github.com>
2024-06-06 01:41:18 +0800

    updated seed script for classes

--- scripts/seed-data/classes.js
+++ scripts/seed-data/classes.js
@@ -1,5 +1,117 @@
 const classes = [
   {
+    class_name: "Hero",
+    region: "Both",
+  },
+  {
+    class_name: "Paladin",
+    region: "Both",
+  },
+  {
+    class_name: "Dark Knight",
+    region: "Both",
+  },
+  {
+    class_name: "Arch Mage (F/P)",
+    region: "Both",
+  },
+  {
+    class_name: "Arch Mage (I/L)",
+    region: "Both",
+  },
+  {
+    class_name: "Bishop",
+    region: "Both",
+  },
+  {
+    class_name: "Bowmaster",
+    region: "GMS",
+  },
+  {
+    class_name: "Bow Master",
+    region: "MSEA",
+  },
+  {
+    class_name: "Marksman",
+    region: "GMS",
+  },
+  {
+    class_name: "Crossbow Master",
+    region: "MSEA",
+  },
+  {
+    class_name: "Pathfinder",
+    region: "Both",
+  },
+  {
+    class_name: "Night Lord",
+    region: "Both",
+  },
+  {
+    class_name: "Dual Blade",
+    region: "Both",
+  },
+  {
+    class_name: "Shadower",
+    region: "Both",
+  },
+  {
+    class_name: "Buccaneer",
+    region: "GMS",
+  },
+  {
+    class_name: "Viper",
+    region: "MSEA",
+  },
+  {
+    class_name: "Corsair",
+    region: "GMS",
+  },
+  {
+    class_name: "Captain",
+    region: "MSEA",
+  },
+  {
+    class_name: "Cannoneer",
+    region: "MSEA",
+  },
+  {
+    class_name: "Cannon Master",
+    region: "MSEA",
+  },
+  {
+    class_name: "Aran",
+    region: "Both",
+  },
+  {
+    class_name: "Evan",
+    region: "Both",
+  },
+  {
+    class_name: "Luminous",
+    region: "Both",
+  },
+  {
+    class_name: "Mercedes",
+    region: "Both",
+  },
+  {
+    class_name: "Phantom",
+    region: "Both",
+  },
+  {
+    class_name: "Shade",
+    region: "GMS",
+  },
+  {
+    class_name: "Eunwol",
+    region: "MSEA",
+  },
+  {
+    class_name: "Night Walker",
+    region: "Both",
+  },
+  {
     class_name: "Thunder Breaker",
     region: "GMS",
   },
@@ -8,8 +120,124 @@ const classes = [
     region: "MSEA",
   },
   {
+    class_name: "Blaze Wizard",
+    region: "GMS",
+  },
+  {
+    class_name: "Flame Wizard",
+    region: "MSEA",
+  },
+  {
+    class_name: "Wind Archer",
+    region: "GMS",
+  },
+  {
+    class_name: "Wind Breaker",
+    region: "MSEA",
+  },
+  {
+    class_name: "Mihile",
+    region: "Both",
+  },
+  {
+    class_name: "Dawn Warrior",
+    region: "GMS",
+  },
+  {
+    class_name: "Soul Master",
+    region: "MSEA",
+  },
+  {
+    class_name: "Demon Avenger",
+    region: "Both",
+  },
+  {
+    class_name: "Demon Slayer",
+    region: "Both",
+  },
+  {
+    class_name: "Xenon",
+    region: "Both",
+  },
+  {
+    class_name: "Blaster",
+    region: "Both",
+  },
+  {
+    class_name: "Mechanic",
+    region: "Both",
+  },
+  {
+    class_name: "Battle Mage",
+    region: "Both",
+  },
+  {
+    class_name: "Wild Hunter",
+    region: "Both",
+  },
+  {
+    class_name: "Kaiser",
+    region: "Both",
+  },
+  {
+    class_name: "Angelic Buster",
+    region: "Both",
+  },
+  {
     class_name: "Cadena",
     region: "Both",
+  },
+  {
+    class_name: "Kain",
+    region: "GMS",
+  },
+  {
+    class_name: "Kaine",
+    region: "MSEA",
+  },
+  {
+    class_name: "Illium",
+    region: "Both",
+  },
+  {
+    class_name: "Ark",
+    region: "Both",
+  },
+  {
+    class_name: "Adele",
+    region: "Both",
+  },
+  {
+    class_name: "Hoyoung",
+    region: "Both",
+  },
+  {
+    class_name: "Lara",
+    region: "Both",
+  },
+  {
+    class_name: "Khali",
+    region: "Both",
+  },
+  {
+    class_name: "Hayato",
+    region: "Both",
+  },
+  {
+    class_name: "Kanna",
+    region: "Both",
+  },
+  {
+    class_name: "Zero",
+    region: "Both",
+  },
+  {
+    class_name: "Kinesis",
+    region: "Both",
+  },
+  {
+    class_name: "Lynn",
+    region: "GMS",
   },
 ];
 
