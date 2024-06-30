f1233b391aff708b08c0d630eb87ef205d048d0e
midorinom <107839253+midorinom@users.noreply.github.com>
2024-06-30 20:37:15 +0800

    added reset button to clear localstorage

---
+++ app/ui/home/dashboard/ResetButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function ResetButton() {
+  function handleClick() {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to wipe all your data on this browser?"
+    );
+    if (confirmDelete) {
+      localStorage.removeItem("userDetails");
+      window.location.reload();
+    }
+  }
+
+  return (
+    <button
+      className="absolute right-4 bottom-4 btn btn-info rounded-full text-xl font-medium text-primary-content"
+      onClick={handleClick}
+    >
+      Clear Data
+    </button>
+  );
+}
