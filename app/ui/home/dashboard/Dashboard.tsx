2e484af15ecbf39e572a5f66a9c63279f48932d5
midorinom <107839253+midorinom@users.noreply.github.com>
2024-06-30 15:47:29 +0800

    Removed Dashboard word

--- app/ui/home/dashboard/Dashboard.tsx
+++ app/ui/home/dashboard/Dashboard.tsx
@@ -1,7 +1,5 @@
 export default function Dashboard() {
   return (
-    <div className="bg-lucid_background bg-cover bg-center h-screen">
-      Dashboard
-    </div>
+    <div className="bg-lucid_background bg-cover bg-center h-screen"></div>
   );
 }
