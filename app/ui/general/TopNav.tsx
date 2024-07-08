0316005187d704678cd9fa96340fbd10f3f14b3a
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 12:49:46 +0800

    done with css for TopNav

--- app/ui/general/TopNav.tsx
+++ app/ui/general/TopNav.tsx
@@ -6,20 +6,40 @@ const rougeScript = Rouge_Script({ subsets: ["latin"], weight: "400" });
 
 export default function TopNav() {
   return (
-    <div className="relative flex w-full h-[6vh] items-center">
+    <div className="relative flex w-full h-[6vh] items-center justify-between ">
       <div className="absolute w-full h-full bg-info opacity-[.70]"></div>
-      <div className="relative w-full h-full flex items-center gap-2">
+      <div className="relative flex items-center gap-2.5 left-3">
         <Image
           src="/general/butterfly_logo.png"
           height={0}
           width={0}
           alt="Butterfly Logo"
           sizes="100vw"
-          className="w-[3.5vw] h-[4vh] ml-3 mb-1.5"
+          className="w-[3.5vw] h-[4vh] mb-1"
         />
-        <div className={`${rougeScript.className} text-primary text-3xl`}>
+        <div
+          className={`${rougeScript.className} text-primary text-3xl mt-0.5`}
+        >
           Dreaming Forest
         </div>
+      </div>
+      <div className="relative flex gap-2.5 right-3">
+        <Image
+          src="/general/profile_icon.png"
+          height={0}
+          width={0}
+          alt="Profile Button"
+          sizes="100vw"
+          className="w-[3.5vw] h-[4vh]"
+        />
+        <Image
+          src="/general/settings_icon.png"
+          height={0}
+          width={0}
+          alt="Settings Button"
+          sizes="100vw"
+          className="w-[3.5vw] h-[4vh]"
+        />
       </div>
     </div>
   );
