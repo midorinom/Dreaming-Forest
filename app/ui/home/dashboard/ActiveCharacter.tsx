743e6314c90641b43d8072a47930bfdefe5ef538
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-09 00:07:24 +0800

    fixed css for naked_char

--- app/ui/home/dashboard/ActiveCharacter.tsx
+++ app/ui/home/dashboard/ActiveCharacter.tsx
@@ -1,17 +1,31 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { ActiveCharacterProps } from "@/app/lib/definitions/dashboard-definitions";
 
 export default function ActiveCharacter({
   activeCharacter,
 }: ActiveCharacterProps) {
+  const [hasImage, setHasImage] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (activeCharacter.image) {
+      setHasImage(true);
+    }
+  }, []);
+
   return (
     <div className="w-full h-full flex justify-center">
-      <div className="w-full h-full flex items-center">
+      <div
+        className={`w-full h-full flex items-center ${
+          !hasImage && "gap-8 ml-8"
+        }`}
+      >
         <Image
           src={
             activeCharacter.imageBase64
               ? activeCharacter.imageBase64
-              : "/general/naked_char.png"
+              : "/general/Midori.png"
           }
           height={0}
           width={0}
@@ -19,9 +33,9 @@ export default function ActiveCharacter({
           sizes="100vw"
           className="h-4/5 w-auto row-span-2 ml-8 justify-self-center"
         />
-        <div className="w-3/5 mt-3 flex flex-col gap-2 justify-center">
-          <div className="text-6xl text-neutral font-medium underline-dreamy-accent underline-offset-8">
-            {activeCharacter.ign}
+        <div className="w-3/5 flex flex-col gap-4 justify-center">
+          <div className="text-6xl text-neutral font-medium underline-dreamy-accent underline-offset-[15px]">
+            Midori
           </div>
           <div className="text-2xl text-neutral flex gap-2">
             <div>Lv {activeCharacter.level}</div>
