fcd042acaea716a0232aace8e0fcfa38f7e4c8ea
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 20:58:59 +0800

    added dailies timer

--- app/ui/home/dashboard/DailiesWeeklies.tsx
+++ app/ui/home/dashboard/DailiesWeeklies.tsx
@@ -1,10 +1,23 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import relativeTime from "dayjs/plugin/relativeTime";
+
 export default function DailiesWeeklies() {
+  dayjs.extend(utc);
+  dayjs.extend(relativeTime);
+  const now = dayjs().utc();
+  const endOfDay = dayjs().utc().endOf("day");
+  const dailiesTimer = endOfDay.diff(now, "hour");
+
   return (
     <div className="flex flex-col items-end mt-2 w-full">
       <div className="w-[36vw] collapse bg-primary">
         <input type="radio" name="accordion" defaultChecked />
         <div className="collapse-title pt-3 text-4xl text-info font-medium underline-dreamy-neutral underline-offset-8">
           Dailies
+        </div>
+        <div className="absolute top-1 right-2 text-2xl text-info">
+          {dailiesTimer}h
         </div>
         <div className="collapse-content max-h-[41vh]"></div>
       </div>
