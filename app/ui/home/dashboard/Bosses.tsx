622c067d59534880d84e8d2a3e868c2a0c3457b2
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 20:05:00 +0800

    added css for bosses

--- app/ui/home/dashboard/Bosses.tsx
+++ app/ui/home/dashboard/Bosses.tsx
@@ -1,3 +1,14 @@
 export default function Bosses() {
-  return <div className="w-full border-4 border-warning">Bosses</div>;
+  return (
+    <div className="w-full">
+      <div className="flex flex-col items-center mt-2 w-full">
+        <div className="w-5/6 bg-base-100 collapse collapse-open">
+          <div className="collapse-title pt-3 text-4xl text-info font-medium underline-dreamy-neutral underline-offset-8">
+            Bosses
+          </div>
+          <div className="collapse-content max-h-[50vh]"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
