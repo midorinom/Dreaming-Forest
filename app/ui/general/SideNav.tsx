4a97e7561f4231af5b18fb8409a4b9f5d94ca9dd
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 17:17:32 +0800

    added click routing functionality to SideNav buttons

--- app/ui/general/SideNav.tsx
+++ app/ui/general/SideNav.tsx
@@ -1,18 +1,26 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function SideNav() {
+  const router = useRouter();
+
+  function handleClick(url: string) {
+    router.replace(url);
+  }
+
   return (
     <div className="row-span-1 col-span-1 row-start-2 col-start-1">
       <div className="relative flex flex-col w-full h-[31vh] items-center justify-around">
-        <div className="absolute w-full h-full bg-accent opacity-[.60] hover:opacity-100"></div>
+        <div className="absolute w-full h-full bg-accent opacity-[.60]"></div>
         <Image
           src="/general/characters_icon.png"
           height={0}
           width={0}
           alt="Characters Button"
           sizes="100vw"
-          className="relative w-[3vw] h-[5vh]"
+          className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+          onClick={() => handleClick("/characters")}
         />
         <Image
           src="/general/dailies_icon.png"
@@ -20,7 +28,8 @@ export default function SideNav() {
           width={0}
           alt="Dailies Button"
           sizes="100vw"
-          className="relative w-[3vw] h-[5vh]"
+          className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+          onClick={() => handleClick("/dailies-weeklies")}
         />
         <Image
           src="/general/bosses_icon.png"
@@ -28,7 +37,8 @@ export default function SideNav() {
           width={0}
           alt="Bosses Button"
           sizes="100vw"
-          className="relative w-[3vw] h-[5vh]"
+          className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+          onClick={() => handleClick("/bosses")}
         />
         <Image
           src="/general/progression_icon.png"
@@ -36,7 +46,8 @@ export default function SideNav() {
           width={0}
           alt="Progression Button"
           sizes="100vw"
-          className="relative w-[3vw] h-[5vh]"
+          className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+          onClick={() => handleClick("/progression")}
         />
       </div>
     </div>
