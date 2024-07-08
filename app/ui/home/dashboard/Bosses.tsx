def47901b8ca63b1caf47e30c7389b0e5084abbf
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 20:28:59 +0800

    adjusted sizes

--- app/ui/home/dashboard/Bosses.tsx
+++ app/ui/home/dashboard/Bosses.tsx
@@ -2,7 +2,7 @@ export default function Bosses() {
   return (
     <div className="w-full">
       <div className="flex flex-col items-center mt-2 w-full">
-        <div className="w-5/6 bg-base-100 collapse collapse-open">
+        <div className="w-4/5 bg-base-100 collapse collapse-open">
           <div className="collapse-title pt-3 text-4xl text-info font-medium underline-dreamy-neutral underline-offset-8">
             Bosses
           </div>
--- app/ui/home/dashboard/DailiesWeeklies.tsx
+++ app/ui/home/dashboard/DailiesWeeklies.tsx
@@ -1,14 +1,14 @@
 export default function DailiesWeeklies() {
   return (
-    <div className="flex flex-col items-center mt-2 w-full">
-      <div className="w-5/6 collapse bg-primary">
+    <div className="flex flex-col items-end mt-2 w-full">
+      <div className="w-[36vw] collapse bg-primary">
         <input type="radio" name="accordion" defaultChecked />
         <div className="collapse-title pt-3 text-4xl text-info font-medium underline-dreamy-neutral underline-offset-8">
           Dailies
         </div>
         <div className="collapse-content max-h-[41vh]"></div>
       </div>
-      <div className="w-5/6 collapse bg-secondary">
+      <div className="w-[36vw] collapse bg-secondary">
         <input type="radio" name="accordion" />
         <div className="collapse-title pt-3 text-4xl text-info font-medium underline-dreamy-neutral underline-offset-8">
           Weeklies
