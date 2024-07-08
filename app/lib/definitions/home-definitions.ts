861ef5afc901f8084f5e822682b4561ab7ab8c90
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 16:20:39 +0800

    refactored into reusable MainApp component to take in different page components

--- app/lib/definitions/home-definitions.ts
+++ app/lib/definitions/home-definitions.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 export type Classes = {
   gms: string[];
   msea: string[];
@@ -5,4 +7,8 @@ export type Classes = {
 
 export type HomeProps = {
   classes: Classes;
+};
+
+export type MainAppProps = {
+  pageComponent: ReactNode;
 };
