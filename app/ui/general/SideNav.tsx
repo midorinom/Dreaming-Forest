fb88c3de0141d870ae1fc8a3d2e621130f9b4457
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-08 17:59:42 +0800

    added SideNav labels and hover states

--- app/ui/general/SideNav.tsx
+++ app/ui/general/SideNav.tsx
@@ -1,36 +1,45 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function SideNav() {
-  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [sideNavHovered, setSideNavHovered] = useState<boolean>(false);
+  const [buttonHovered, setButtonHovered] = useState<string>("");
   const router = useRouter();
 
   function handleClick(url: string) {
     router.replace(url);
   }
 
+  useEffect(() => {
+    if (buttonHovered) {
+    }
+  }, [buttonHovered]);
+
   return (
     <div className="row-span-1 col-span-1 row-start-2 col-start-1">
       <div
         className="relative flex flex-col w-full h-[31vh] items-center justify-around"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        onMouseEnter={() => setSideNavHovered(true)}
+        onMouseLeave={() => setSideNavHovered(false)}
       >
         <div
           className={`absolute w-full h-full bg-accent ${
-            isHovered ? "opacity-100" : "opacity-[.60]"
+            sideNavHovered ? "opacity-100" : "opacity-[.60]"
           }`}
         ></div>
         <Image
           src="/general/characters_icon.png"
+          id="characters"
           height={0}
           width={0}
           alt="Characters Button"
           sizes="100vw"
           className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
           onClick={() => handleClick("/characters")}
+          onMouseEnter={() => setButtonHovered("characters")}
+          onMouseLeave={() => setButtonHovered("")}
         />
         <Image
           src="/general/dailies_icon.png"
@@ -40,6 +49,8 @@ export default function SideNav() {
           sizes="100vw"
           className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
           onClick={() => handleClick("/dailies-weeklies")}
+          onMouseEnter={() => setButtonHovered("dailiesWeeklies")}
+          onMouseLeave={() => setButtonHovered("")}
         />
         <Image
           src="/general/bosses_icon.png"
@@ -49,6 +60,8 @@ export default function SideNav() {
           sizes="100vw"
           className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
           onClick={() => handleClick("/bosses")}
+          onMouseEnter={() => setButtonHovered("bosses")}
+          onMouseLeave={() => setButtonHovered("")}
         />
         <Image
           src="/general/progression_icon.png"
@@ -58,7 +71,29 @@ export default function SideNav() {
           sizes="100vw"
           className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
           onClick={() => handleClick("/progression")}
+          onMouseEnter={() => setButtonHovered("progression")}
+          onMouseLeave={() => setButtonHovered("")}
         />
+        {buttonHovered === "characters" && (
+          <div className="absolute top-[1.5vh] left-[4vw] w-fit-content h-[5vh] bg-accent text-2xl text-info font-medium flex items-center p-2">
+            Characters
+          </div>
+        )}
+        {buttonHovered === "dailiesWeeklies" && (
+          <div className="absolute top-[9.3vh] left-[4vw] w-fit-content h-[5vh] bg-accent text-2xl text-info font-medium flex items-center p-2">
+            Dailies/Weeklies
+          </div>
+        )}
+        {buttonHovered === "bosses" && (
+          <div className="absolute top-[17vh] left-[4vw] w-fit-content h-[5vh] bg-accent text-2xl text-info font-medium flex items-center p-2">
+            Bosses
+          </div>
+        )}
+        {buttonHovered === "progression" && (
+          <div className="absolute top-[24.7vh] left-[4vw] w-fit-content h-[5vh] bg-accent text-2xl text-info font-medium flex items-center p-2">
+            Progression
+          </div>
+        )}
       </div>
     </div>
   );
