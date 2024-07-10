5521002a15fa50aa8c75b969397a4c03448b6541
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-11 02:08:49 +0800

    duplicated RootLayout in welcome ddirectory to fix bug

--- app/welcome/layout.tsx
+++ app/welcome/layout.tsx
@@ -1,11 +1,21 @@
-export default function WelcomeLayout({
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "@/app/ui/globals.css";
+
+export const metadata: Metadata = {
+  title: "Dreaming Forest",
+  description: "Maplestory App",
+};
+const outfit = Outfit({ subsets: ["latin"], weight: "400" });
+
+export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   );
 }
