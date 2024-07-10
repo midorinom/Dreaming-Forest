0d27f5fc6c8ef20e7e0c3ef000c164ef18b551b3
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-11 03:07:13 +0800

    refactored router in navbars to Link

--- app/ui/general/SideNav.tsx
+++ app/ui/general/SideNav.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { useState } from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SideNav() {
   const [buttonHovered, setButtonHovered] = useState<string>("");
-  const router = useRouter();
   const pathname = usePathname();
   const showNav = pathname !== "/welcome";
-
-  function handleClick(url: string) {
-    router.replace(url);
-  }
 
   return (
     showNav && (
@@ -22,51 +18,55 @@ export default function SideNav() {
               buttonHovered ? "opacity-100" : "opacity-[.60]"
             }`}
           ></div>
-          <Image
-            src="/general/characters_icon.png"
-            id="characters"
-            height={0}
-            width={0}
-            alt="Characters Button"
-            sizes="100vw"
-            className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
-            onClick={() => handleClick("/characters")}
-            onMouseEnter={() => setButtonHovered("characters")}
-            onMouseLeave={() => setButtonHovered("")}
-          />
-          <Image
-            src="/general/dailies_icon.png"
-            height={0}
-            width={0}
-            alt="Dailies Button"
-            sizes="100vw"
-            className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
-            onClick={() => handleClick("/dailies-weeklies")}
-            onMouseEnter={() => setButtonHovered("dailiesWeeklies")}
-            onMouseLeave={() => setButtonHovered("")}
-          />
-          <Image
-            src="/general/bosses_icon.png"
-            height={0}
-            width={0}
-            alt="Bosses Button"
-            sizes="100vw"
-            className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
-            onClick={() => handleClick("/bosses")}
-            onMouseEnter={() => setButtonHovered("bosses")}
-            onMouseLeave={() => setButtonHovered("")}
-          />
-          <Image
-            src="/general/progression_icon.png"
-            height={0}
-            width={0}
-            alt="Progression Button"
-            sizes="100vw"
-            className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
-            onClick={() => handleClick("/progression")}
-            onMouseEnter={() => setButtonHovered("progression")}
-            onMouseLeave={() => setButtonHovered("")}
-          />
+          <Link href="/characters">
+            <Image
+              src="/general/characters_icon.png"
+              id="characters"
+              height={0}
+              width={0}
+              alt="Characters Button"
+              sizes="100vw"
+              className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+              onMouseEnter={() => setButtonHovered("characters")}
+              onMouseLeave={() => setButtonHovered("")}
+            />
+          </Link>
+          <Link href="/dailies-weeklies">
+            <Image
+              src="/general/dailies_icon.png"
+              height={0}
+              width={0}
+              alt="Dailies Button"
+              sizes="100vw"
+              className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+              onMouseEnter={() => setButtonHovered("dailiesWeeklies")}
+              onMouseLeave={() => setButtonHovered("")}
+            />
+          </Link>
+          <Link href="/bosses">
+            <Image
+              src="/general/bosses_icon.png"
+              height={0}
+              width={0}
+              alt="Bosses Button"
+              sizes="100vw"
+              className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+              onMouseEnter={() => setButtonHovered("bosses")}
+              onMouseLeave={() => setButtonHovered("")}
+            />
+          </Link>
+          <Link href="/progression">
+            <Image
+              src="/general/progression_icon.png"
+              height={0}
+              width={0}
+              alt="Progression Button"
+              sizes="100vw"
+              className="relative w-[3vw] h-[5vh] hover:cursor-pointer"
+              onMouseEnter={() => setButtonHovered("progression")}
+              onMouseLeave={() => setButtonHovered("")}
+            />
+          </Link>
           {buttonHovered === "characters" && (
             <div className="absolute top-[1.5vh] z-10 left-[4vw] w-fit-content h-[5vh] bg-accent text-2xl text-info font-medium flex items-center p-2">
               Characters
