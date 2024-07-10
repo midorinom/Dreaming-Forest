0acc37587764bcb2055d7d2ccdff3ce84e81952b
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-11 00:37:57 +0800

    moved import statement

--- app/ui/general/SideNav.tsx
+++ app/ui/general/SideNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function SideNav() {
   const [buttonHovered, setButtonHovered] = useState<string>("");
