33558561e1a383e66469a59266223e2c1e283fd8
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-12 10:37:35 +0800

    prevented layout shifting and also added loading spinner

--- app/ui/dashboard/ActiveCharacter.tsx
+++ app/ui/dashboard/ActiveCharacter.tsx
@@ -15,33 +15,41 @@ export default function ActiveCharacter({ userDetails }: ActiveCharacterProps) {
 
   return (
     <div className="w-full h-full flex justify-center">
-      <div
-        className={`w-full h-full flex items-center ${
-          !activeCharacter.image && "gap-8 ml-8"
-        }`}
-      >
-        {/* <span className="loading loading-spinner text-accent"></span> */}
-        <Image
-          src={
-            activeCharacter.image
-              ? activeCharacter.image
-              : "/general/naked_char.png"
-          }
-          height={0}
-          width={0}
-          alt="Active Character"
-          sizes="100vw"
-          className="h-4/5 w-auto ml-8 justify-self-center"
-        />
-        <div className="w-3/5 flex flex-col gap-4 justify-center">
-          <div className="text-6xl text-neutral font-medium underline-dreamy-accent underline-offset-[15px]">
-            {activeCharacter.ign}
-          </div>
-          <div className="text-2xl text-neutral flex gap-2">
-            <div>Lv {activeCharacter.level}</div>
-            <div> {activeCharacter.maplestoryClass}</div>
-          </div>
-        </div>
+      <div className={"w-full h-full flex items-center gap-4 ml-8"}>
+        {!activeCharacter.ign ? (
+          <span className="mx-auto h-1/2 w-auto loading loading-spinner text-accent"></span>
+        ) : (
+          <>
+            <div
+              style={{
+                position: "relative",
+              }}
+              className="h-4/5 w-[28%] ml-8"
+            >
+              <Image
+                src={
+                  activeCharacter.image
+                    ? activeCharacter.image
+                    : "/general/naked_char.png"
+                }
+                height={0}
+                width={0}
+                alt="Active Character"
+                sizes="100vw"
+                className="absolute h-full w-full"
+              />
+            </div>
+            <div className="w-3/5 flex flex-col gap-4 justify-center">
+              <div className="text-6xl text-neutral font-medium underline-dreamy-accent underline-offset-[15px]">
+                {activeCharacter.ign}
+              </div>
+              <div className="text-2xl text-neutral flex gap-2">
+                <div>Lv {activeCharacter.level}</div>
+                <div> {activeCharacter.maplestoryClass}</div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
