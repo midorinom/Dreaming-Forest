c18cb449544212fc1868796b22f59158df6992b0
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-14 20:04:19 +0800

    added conditional rendering to bosses

--- app/ui/dashboard/Bosses.tsx
+++ app/ui/dashboard/Bosses.tsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
 import type { BossesProps } from "@/app/lib/definitions/dashboard-definitions";
+import { Boss } from "@/app/lib/definitions/general-definitions";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import isoWeek from "dayjs/plugin/isoWeek";
 
 export default function Bosses({ region, activeCharacter }: BossesProps) {
+  const [bosses, setBosses] = useState<Boss[]>([]);
   const [bossesTimer, setBossesTimer] = useState<string>("");
 
   useEffect(() => {
+    // Set Dailies and Weeklies
+    setBosses(activeCharacter.bosses);
+
+    // Set Dailies Timer
     dayjs.extend(utc);
     dayjs.extend(isoWeek);
     let now = undefined;
@@ -53,13 +59,21 @@ export default function Bosses({ region, activeCharacter }: BossesProps) {
     <div className="w-full">
       <div className="flex flex-col items-center w-full mt-2">
         <div className="w-4/5 bg-base-100 collapse collapse-open">
-          <div className="pt-3 text-4xl font-medium collapse-title text-info underline-dreamy-neutral underline-offset-8">
+          <div
+            className={`${
+              bosses.length === 0 && "mb-1"
+            } pt-3 text-4xl font-medium collapse-title text-info underline-dreamy-neutral underline-offset-8`}
+          >
             Bosses
           </div>
-          <div className="absolute text-2xl top-1 right-2 text-info">
-            {bossesTimer}
-          </div>
-          <div className="collapse-content max-h-[50vh]"></div>
+          {bossesTimer && bosses.length > 0 && (
+            <div className="absolute text-2xl top-1 right-2 text-info">
+              {bossesTimer}
+            </div>
+          )}
+          {bosses.length > 0 && (
+            <div className="collapse-content max-h-[50vh]"></div>
+          )}
         </div>
       </div>
     </div>
