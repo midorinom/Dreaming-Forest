66a8109c6ac8423bfb06bf63f4ec83c1f49be0bb
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-15 11:31:42 +0800

    added heading hover to show edit button

--- app/ui/dashboard/Dailies.tsx
+++ app/ui/dashboard/Dailies.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import type { DailiesProps } from "@/app/lib/definitions/dashboard-definitions";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 
 export default function Dailies({ region, dailies, weeklies }: DailiesProps) {
   const [dailiesTimer, setDailiesTimer] = useState<string>("");
+  const [headingHovered, setHeadingHovered] = useState<boolean>(false);
 
   useEffect(() => {
     dayjs.extend(utc);
@@ -32,7 +34,7 @@ export default function Dailies({ region, dailies, weeklies }: DailiesProps) {
 
   return (
     <div
-      className={`w-[36vw] collapse ${
+      className={`collapse w-[36vw] ${
         (weeklies.length === 0 || dailies.length === 0) && "collapse-open"
       } bg-primary`}
     >
@@ -42,12 +44,24 @@ export default function Dailies({ region, dailies, weeklies }: DailiesProps) {
       <div
         className={`${
           dailies.length === 0 && "mb-1"
-        } text-4xl font-medium collapse-title text-info underline-dreamy-neutral pt-3 underline-offset-8`}
+        } collapse-title relative pt-3 text-4xl font-medium text-info underline-offset-8 underline-dreamy-neutral`}
+        onMouseEnter={() => setHeadingHovered(true)}
+        onMouseLeave={() => setHeadingHovered(false)}
       >
         Dailies
+        {headingHovered && (
+          <Image
+            src="/general/ui_icons/edit_icon.png"
+            height={0}
+            width={0}
+            alt="Progression Button"
+            sizes="100vw"
+            className="absolute left-0 top-0 h-[5vh] w-[3vw] hover:cursor-pointer"
+          />
+        )}
       </div>
       {dailiesTimer && dailies.length > 0 && (
-        <div className="absolute text-2xl top-1 right-2 text-info">
+        <div className="absolute right-2 top-1 text-2xl text-info">
           {dailiesTimer}
         </div>
       )}
