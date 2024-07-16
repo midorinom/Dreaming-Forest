7a5b1a9fb427da51f5ba8b175a58c9c42359eedc
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-16 15:13:49 +0800

    created schema for BossesInfo

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,25 +1,41 @@
-import { pgTable, varchar, uuid, integer } from "drizzle-orm/pg-core";
+import { pgTable, varchar, uuid, integer, bigint } from "drizzle-orm/pg-core";
 
 export const Regions = pgTable("regions", {
-  region: varchar("region").primaryKey(),
+  region: varchar("region").primaryKey().notNull(),
 });
 
 export const Classes = pgTable("classes", {
-  class_name: varchar("class_name").primaryKey(),
-  region: varchar("region").references(() => Regions.region),
+  class_name: varchar("class_name").primaryKey().notNull(),
+  region: varchar("region")
+    .references(() => Regions.region)
+    .notNull(),
+});
+
+export const BossesInfo = pgTable("characters", {
+  bosses_position: integer("bosses_position").primaryKey().notNull(),
+  bosses_image: varchar("bosses_image").notNull(),
+  dashboard_position: integer("dashboard_position").notNull(),
+  dashboard_image: varchar("dashboard_image").notNull(),
+  meso: bigint("meso", { mode: "number" }).notNull(),
 });
 
 export const Users = pgTable("users", {
-  username: varchar("class_name").primaryKey(),
-  region: varchar("region").references(() => Regions.region),
+  username: varchar("class_name").primaryKey().notNull(),
+  region: varchar("region")
+    .references(() => Regions.region)
+    .notNull(),
   pw_hash: varchar("pw_hash").notNull(),
 });
 
 export const Characters = pgTable("characters", {
-  id: uuid("id").primaryKey(),
-  username: varchar("username").references(() => Users.username),
+  id: uuid("id").primaryKey().notNull(),
+  username: varchar("username")
+    .references(() => Users.username)
+    .notNull(),
   ign: varchar("ign").notNull(),
   level: integer("level").notNull(),
-  class_name: varchar("class_name").references(() => Classes.class_name),
+  class_name: varchar("class_name")
+    .references(() => Classes.class_name)
+    .notNull(),
   image: uuid("image").notNull(),
 });
