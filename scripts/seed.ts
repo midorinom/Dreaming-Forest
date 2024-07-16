bfdc876e616b5c7b6d041d00b05c1e075190b71a
midorinom <107839253+midorinom@users.noreply.github.com>
2024-07-16 15:13:58 +0800

    created seed file for bossesInfo

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 import { sql } from "@vercel/postgres";
-import { Regions, Classes } from "@/drizzle/schema";
+import { Regions, Classes, BossesInfo } from "@/drizzle/schema";
 import { regions } from "./seed-data/regions";
 import { classes } from "./seed-data/classes";
+import { bossesInfo } from "./seed-data/bosses_info";
 
 const db = drizzle(sql);
 
@@ -41,15 +42,33 @@ async function seedClasses() {
   }
 }
 
+async function seedBossesInfo() {
+  try {
+    const insertedBossesInfo = await db
+      .insert(BossesInfo)
+      .values(bossesInfo)
+      .onConflictDoNothing();
+    console.log(`Successfully seeded bosses_info`);
+
+    return {
+      bossesInfo: insertedBossesInfo,
+    };
+  } catch (error) {
+    console.error("Error seeding bosses_info:", error);
+    throw error;
+  }
+}
+
 export async function seedDatabase() {
   try {
     await migrate(db, { migrationsFolder: "drizzle" });
     await seedRegions();
     await seedClasses();
+    await seedBossesInfo();
   } catch (error) {
     console.error(
       "An error occurred while attempting to seed the database:",
-      error
+      error,
     );
     throw error;
   }
