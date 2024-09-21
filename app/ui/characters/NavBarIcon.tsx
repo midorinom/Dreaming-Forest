29e4a32253163d40f381907ccf331ac927e3781d
Hao Fang <haofang97@gmail.com>
2024-09-21 17:52:56 +0800

    made NavBarIcon dynamic according to icon prop

--- app/ui/characters/NavBarIcon.tsx
+++ app/ui/characters/NavBarIcon.tsx
@@ -7,12 +7,31 @@ export default function NavBarIcon({
   setCurrentPage,
   setButtonHovered,
 }: NavBarIconProps) {
+  let imageUrl = "/general/ui_icons/view_characters_icon.png";
+
+  switch (icon) {
+    case "rearrange":
+      imageUrl = "/general/ui_icons/rearrange_icon.png";
+      break;
+
+    case "add":
+      imageUrl = "/general/ui_icons/add_character_icon.png";
+      break;
+
+    case "delete":
+      imageUrl = "/general/ui_icons/delete_character_icon.png";
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <Image
-      src="/general/ui_icons/rearrange_icon.png"
+      src={imageUrl}
       height={0}
       width={0}
-      alt="Rearrange Button"
+      alt={`${icon} Characters Button`}
       sizes="100vw"
       className="relative h-[5vh] w-[3vw] hover:cursor-pointer"
       onMouseEnter={() => setButtonHovered(true)}
