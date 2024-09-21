a873a4f498d32b7ea2c1ec7ad4ac5d814341e97c
Hao Fang <haofang97@gmail.com>
2024-09-21 17:55:58 +0800

    removed unused import

--- app/ui/characters/NavBar.tsx
+++ app/ui/characters/NavBar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
 import { NavBarProps } from "@/app/lib/definitions/characters-definitions";
 import NavBarIcon from "./NavBarIcon";
 
