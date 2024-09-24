b5526963600a54148c049d048a3e810ff02e60d9
midorinom <107839253+midorinom@users.noreply.github.com>
2024-09-24 22:02:31 +0800

    added CharactersWheel css and hover logic

--- app/ui/dashboard/CharactersWheel.tsx
+++ app/ui/dashboard/CharactersWheel.tsx
@@ -1,4 +1,32 @@
 "use client";
-export default function CharactersWheel() {
-  return <div></div>;
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { CharactersWheelProps } from "@/app/lib/definitions/dashboard-definitions";
+
+export default function CharactersWheel({
+  activeCharacter,
+  setActiveCharacter,
+}: CharactersWheelProps) {
+  const [wheelHovered, setWheelHovered] = useState<boolean>(false);
+
+  return (
+    <div
+      className="relative flex items-center justify-center"
+      onMouseLeave={() => setWheelHovered(false)}
+    >
+      {wheelHovered ? (
+        <div>Test</div>
+      ) : (
+        <Image
+          src={"/general/ui_icons/switch_characters_icon.png"}
+          height={0}
+          width={0}
+          alt="Active Character"
+          sizes="100vw"
+          className="absolute mt-2 h-[40%] w-auto hover:cursor-pointer"
+          onMouseEnter={() => setWheelHovered(true)}
+        />
+      )}
+    </div>
+  );
 }
