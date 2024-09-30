c783daaeae12c8bb878ed2a0aeb43631d4a6afd8
midorinom <107839253+midorinom@users.noreply.github.com>
2024-09-30 19:20:00 +0800

    implemented CharacterCard mapping based on currentPagePagination

--- app/ui/characters/view-characters/ViewCharacters.tsx
+++ app/ui/characters/view-characters/ViewCharacters.tsx
@@ -1,28 +1,40 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import type { ViewCharactersProps } from "@/app/lib/definitions/characters-definitions";
-import type { Character } from "@/app/lib/definitions/general-definitions";
 import CharacterCard from "./CharacterCard";
 
 export default function ViewCharacters({
   charactersProp,
   currentPagePagination,
 }: ViewCharactersProps) {
-  const [characters, setCharacters] = useState<Character[]>(charactersProp);
+  const [characterCards, setCharacterCards] = useState<ReactElement[]>([]);
+
+  useEffect(() => {
+    if (charactersProp.length > 0) {
+      const characterCardsArray: ReactElement[] = [];
+      const firstIndex = (currentPagePagination - 1) * 4;
+      const lastIndex = Math.min(
+        4 * currentPagePagination,
+        charactersProp.length,
+      );
+
+      for (let i = firstIndex; i < lastIndex; i++) {
+        characterCardsArray.push(
+          <CharacterCard
+            key={charactersProp[i].characterId}
+            characterProp={charactersProp[i]}
+          />,
+        );
+      }
+
+      setCharacterCards(characterCardsArray);
+    }
+  }, [charactersProp, currentPagePagination]);
 
   return (
     <div className="grid grid-cols-2 grid-rows-2 items-center justify-items-center gap-y-6">
-      {characters &&
-        characters.map((character, index) => {
-          if (index < 4 * currentPagePagination) {
-            return (
-              <CharacterCard
-                key={character.characterId}
-                characterProp={character}
-              />
-            );
-          }
-        })}
+      {characterCards.length > 0 &&
+        characterCards.map((charactersCard) => charactersCard)}
     </div>
   );
 }
