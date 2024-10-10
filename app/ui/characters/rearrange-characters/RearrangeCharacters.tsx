c1735ef0636ffbe24968c4c14daee25a1b414562
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-10 18:14:56 +0800

    formatting

--- app/ui/characters/rearrange-characters/RearrangeCharacters.tsx
+++ app/ui/characters/rearrange-characters/RearrangeCharacters.tsx
@@ -2,12 +2,10 @@
 import { RearrangeCharactersProps } from "@/app/lib/definitions/characters-definitions";
 import Image from "next/image";
 
-export default function RearrangeCharacters(
-  {
-    charactersProp,
-    currentPagePagination,
-  }: RearrangeCharactersProps
-) {
+export default function RearrangeCharacters({
+  charactersProp,
+  currentPagePagination,
+}: RearrangeCharactersProps) {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex h-full w-full items-center justify-center gap-2">
