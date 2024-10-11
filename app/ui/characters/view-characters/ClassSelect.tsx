120a0d60a900fb1047a90ec79a5207703c04b15a
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-11 17:35:52 +0800

    made ClassSelect colour dynamic based on background colour

--- app/ui/characters/view-characters/ClassSelect.tsx
+++ app/ui/characters/view-characters/ClassSelect.tsx
@@ -4,7 +4,11 @@ import { useCharacters } from "@/app/ui/contexts/CharactersContext";
 import AutoComplete from "@/app/ui/general/AutoComplete";
 import type { ClassFieldProps } from "@/app/lib/definitions/characters-definitions";
 
-const ClassSelect = ({ setMaplestoryClass, isTopCard }: ClassFieldProps) => {
+const ClassSelect = ({
+  setMaplestoryClass,
+  isTopCard,
+  isPrimaryBackground,
+}: ClassFieldProps) => {
   const charactersCtx = useCharacters();
   const [classInput, setClassInput] = useState<string>("");
   const [classes, setClasses] = useState<string[]>([]);
@@ -14,12 +18,10 @@ const ClassSelect = ({ setMaplestoryClass, isTopCard }: ClassFieldProps) => {
   const dropdown_className = {
     "dropdown w-full": true,
   };
-  const input_className =
-    "grow block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-base text-primary-content bg-neutral border-0 border-b-2 border-secondary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer";
-  const label_className =
-    "absolute text-base text-secondary duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto";
+  const input_className = `grow block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-base text-primary-content bg-neutral border-0 border-b-2 ${isPrimaryBackground ? "border-secondary" : "border-primary"} appearance-none focus:outline-none focus:ring-0 ${isPrimaryBackground ? "focus:border-secondary" : "focus:border-primary"} peer`;
+  const label_className = `absolute text-base ${isPrimaryBackground ? "text-secondary" : "text-primary"} duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 ${isPrimaryBackground ? "peer-focus:text-secondary" : "peer-focus:text-primary"} peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`;
   const dropdown_content_className = `dropdown-content bg-base-200 ${isTopCard ? "top-14" : "bottom-14"} max-h-40 overflow-scroll scrollbar-hide flex-col rounded-md z-10`;
-  const ul_className = "menu bg-secondary";
+  const ul_className = `menu ${isPrimaryBackground ? "bg-secondary" : "bg-primary"}`;
   const li_className = "border-b border-b-base-content/10 w-full";
 
   useEffect(() => {
