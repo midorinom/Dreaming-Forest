7ef3bf2296f2856732845d66e4dd40bb99a655f8
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-11 17:25:52 +0800

    made IgnField colour dynamic based on background colour

--- app/ui/characters/view-characters/IgnField.tsx
+++ app/ui/characters/view-characters/IgnField.tsx
@@ -30,7 +30,9 @@ export default function IgnField({
         className={`block w-full grow appearance-none rounded-t-lg border-0 border-b-2 bg-neutral px-2.5 pb-2.5 pt-5 text-base text-primary-content ${
           isInvalid
             ? "border-error focus:border-error"
-            : "border-secondary focus:border-secondary"
+            : isPrimaryBackground
+              ? "border-secondary focus:border-secondary"
+              : "border-primary focus:border-primary"
         } peer focus:outline-none focus:ring-0`}
         placeholder=""
         maxLength={12}
@@ -41,7 +43,9 @@ export default function IgnField({
         className={`absolute text-base ${
           isInvalid
             ? "text-error peer-focus:text-error"
-            : "text-secondary peer-focus:text-secondary"
+            : isPrimaryBackground
+              ? "text-secondary peer-focus:text-secondary"
+              : "text-primary peer-focus:text-primary"
         } start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4`}
       >
         IGN
