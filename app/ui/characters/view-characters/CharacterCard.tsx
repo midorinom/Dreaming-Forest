78cf65acefb075311d1f27db0df5279f99c5b071
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-11 16:13:03 +0800

    added hovering logic for edit button

--- app/ui/characters/view-characters/CharacterCard.tsx
+++ app/ui/characters/view-characters/CharacterCard.tsx
@@ -12,6 +12,7 @@ import CharacterTracking from "./CharacterTracking";
 export default function CharacterCard({ characterProp }: CharacterCardProps) {
   const isMounted = useRef(false);
   const [character, setCharacter] = useState<Character>(characterProp);
+  const [headingHovered, setHeadingHovered] = useState<boolean>(false);
 
   function isPrimaryBackground(): boolean {
     if (character.position % 4 === 0 || character.position % 4 === 3) {
@@ -41,15 +42,19 @@ export default function CharacterCard({ characterProp }: CharacterCardProps) {
       {character && (
         <div
           className={`relative grid h-[84%] w-[83%] grid-rows-[24vh_1fr] justify-items-center ${character.position % 4 === 0 || character.position % 4 === 1 ? "self-end" : "self-start"} rounded-3xl ${isPrimaryBackground() ? "bg-primary/75" : "bg-secondary/75"}`}
+          onMouseEnter={() => setHeadingHovered(true)}
+          onMouseLeave={() => setHeadingHovered(false)}
         >
-          <Image
-            src="/general/ui_icons/edit_icon.png"
-            height={0}
-            width={0}
-            alt="Edit Button"
-            sizes="100vw"
-            className="absolute right-2 top-2 h-[3rem] w-[auto] hover:cursor-pointer"
-          />
+          {headingHovered && (
+            <Image
+              src="/general/ui_icons/edit_icon.png"
+              height={0}
+              width={0}
+              alt="Edit Button"
+              sizes="100vw"
+              className="absolute right-2 top-2 h-[3rem] w-[auto] hover:cursor-pointer"
+            />
+          )}
           <CharacterDetails character={character} />
           <CharacterTracking
             trackingProp={character.tracking}
