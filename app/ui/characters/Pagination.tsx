d9f9e1b5dd0df62bcf5c778d0b8f3d98b9390213
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-18 16:42:41 +0800

    added pagination to RearrangeCharacters

--- app/ui/characters/Pagination.tsx
+++ app/ui/characters/Pagination.tsx
@@ -42,10 +42,9 @@ export default function Pagination({
 
   return (
     <div className="join join-vertical my-auto flex -translate-y-[3.5vh] flex-col">
-      {(currentPage === "view" || currentPage === "delete") &&
-        totalPagesPagination > 1 && (
-          <>{paginationButtons.map((paginationButton) => paginationButton)}</>
-        )}
+      {currentPage !== "add" && totalPagesPagination > 1 && (
+        <>{paginationButtons.map((paginationButton) => paginationButton)}</>
+      )}
     </div>
   );
 }
