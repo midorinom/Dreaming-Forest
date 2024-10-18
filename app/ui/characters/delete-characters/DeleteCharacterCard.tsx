b01fb169976ad40f6d9749ac2477db5729fb2640
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-18 15:24:19 +0800

    added css mockkup for DeleteCharacterCard

--- app/ui/characters/delete-characters/DeleteCharacterCard.tsx
+++ app/ui/characters/delete-characters/DeleteCharacterCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { DeleteCharacterCardProps } from "@/app/lib/definitions/characters-definitions";
 
 export default function DeleteCharacterCard({
@@ -6,5 +7,35 @@ export default function DeleteCharacterCard({
   characters,
   setCharacters,
 }: DeleteCharacterCardProps) {
-  return <div>Test</div>;
+  return (
+    <div className="flex h-4/5 w-full flex-col items-center justify-center">
+      <div
+        style={{
+          position: "relative",
+        }}
+        className="flex h-full w-full items-center justify-center"
+      >
+        <Image
+          src={
+            characterProp.image
+              ? characterProp.image
+              : "/general/naked_char.png"
+          }
+          height={0}
+          width={0}
+          alt="Character Image"
+          sizes="100vw"
+          className="absolute h-full w-auto"
+        />
+      </div>
+      <Image
+        src="/general/ui_icons/delete_icon.png"
+        height={0}
+        width={0}
+        alt="Delete Button"
+        sizes="100vw"
+        className="h-2/5 w-auto hover:cursor-pointer"
+      />
+    </div>
+  );
 }
