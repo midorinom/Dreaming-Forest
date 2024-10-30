88b92a493edc91badcabc0119e4974fcbf3d6adb
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-30 17:39:15 +0800

    changed small spirit dialogue for welcome and select_region

--- public/welcome/small_spirit.ts
+++ public/welcome/small_spirit.ts
@@ -2,7 +2,8 @@ import type { Dialogue } from "@/app/lib/definitions/welcome-definitions";
 
 const smallSpiritDialogue: Dialogue = {
   welcome:
-    "Welcome to Dreaming Forest, a Maplestory tool to help you keep track of stuff! To get started, select your region and add a character.",
+    "Welcome to Dreaming Forest, a Maplestory tool to help you keep track of stuff! Do login if you already have an account.",
+  select_region: "To get started, select your region.",
   add_character:
     "Add a character that you want to track. You can get an image of your character from maplestory.gg, nexon rankings page or maples.im",
   create_account:
@@ -12,6 +13,7 @@ const smallSpiritDialogue: Dialogue = {
 
 const smallSpiritImage: Dialogue = {
   welcome: "/welcome/small_spirit_happy.png",
+  select_region: "/welcome/small_spirit_happy.png",
   add_character: "/welcome/small_spirit_smiling.png",
   create_account: "/welcome/small_spirit_smiling.png",
   uploading: "/welcome/small_spirit_neutral.png",
