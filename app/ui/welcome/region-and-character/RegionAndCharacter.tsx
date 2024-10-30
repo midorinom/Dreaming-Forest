5ca1f22808edd2d489b7ae4372b652c409ff837a
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-30 17:09:52 +0800

    changed alt text

--- app/ui/welcome/region-and-character/RegionAndCharacter.tsx
+++ app/ui/welcome/region-and-character/RegionAndCharacter.tsx
@@ -96,7 +96,7 @@ export default function RegionAndCharacter({
         >
           <img
             src="/general/ui_icons/butterfly_logo.png"
-            alt="My Icon"
+            alt="Butterfly Logo"
             className="h-7 w-7"
           />
           Proceed
