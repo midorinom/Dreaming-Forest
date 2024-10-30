93bd4cfa90004e7140a6fd58890ff57467e986c5
midorinom <107839253+midorinom@users.noreply.github.com>
2024-10-30 17:10:05 +0800

    added skeletal UI to NewUser

--- app/ui/welcome/new-user/NewUser.tsx
+++ app/ui/welcome/new-user/NewUser.tsx
@@ -1,6 +1,41 @@
 "use client";
+import { useState } from "react";
+import UsernameField from "./UsernameField";
+import PasswordField from "./PasswordField";
 
 // TODO: option to either login or create a new account
 export default function NewUser() {
-  return <div>NewUser</div>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div className="flex w-1/4 flex-col items-center">
+      <div className="collapse collapse-open flex min-h-48 flex-col items-center gap-3 bg-base-100 pt-6">
+        <div className="flex flex-col gap-8">
+          <UsernameField setUsername={setUsername} />
+          {/* <PasswordField setPassword={setPassword} /> */}
+        </div>
+      </div>
+      <div className="mt-5 flex w-full justify-between">
+        <button
+          className="btn btn-secondary btn-lg text-xl font-medium text-primary-content"
+          // onClick={() => setDone(true)}
+        >
+          I am new!
+        </button>
+        <button
+          className="btn glass btn-accent btn-lg rounded-full text-xl font-medium text-primary-content"
+          disabled={username && password ? false : true}
+          // onClick={() => setDone(true)}
+        >
+          <img
+            src="/general/ui_icons/butterfly_logo.png"
+            alt="Butterfly Logo"
+            className="h-7 w-7"
+          />
+          Login
+        </button>
+      </div>
+    </div>
+  );
 }
