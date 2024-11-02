9d3413cf083ffe8939a3573365e66a282032a684
Hao Fang <haofang97@gmail.com>
2024-11-02 18:42:20 +0800

    removed password hashing on client side

--- app/ui/welcome/Welcome.tsx
+++ app/ui/welcome/Welcome.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { v4 as uuidv4 } from "uuid";
 import type { UUID } from "crypto";
-import bcrypt from "bcryptjs";
 import type {
   DialogueIndex,
   WelcomeProps,
@@ -110,22 +109,13 @@ export default function Welcome({ classes }: WelcomeProps) {
     }
 
     async function updateDatabase(newUser: User) {
-      const hashedPassword = await hashPassword(password);
-
       await fetch(`/api/users`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ user: newUser, hashedPassword: hashedPassword }),
+        body: JSON.stringify({ user: newUser, password: password }),
       });
-    }
-
-    async function hashPassword(plainPassword: string) {
-      const saltRounds = 10; // Defines the complexity of the hashing
-      const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
-
-      return hashedPassword;
     }
   }, [done]);
 
