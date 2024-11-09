cbcbf1377f15ddbc23ae78d8b52d1382ac2455bb
midorinom <107839253+midorinom@users.noreply.github.com>
2024-11-10 04:46:09 +0800

    search username by lowercase

--- app/lib/fetches/welcome-fetches.ts
+++ app/lib/fetches/welcome-fetches.ts
@@ -1,12 +1,12 @@
 import { UUID } from "crypto";
 
-export async function fetchUserId(username: string): Promise<UUID> {
+export async function fetchUserId(username: string): Promise<UUID | undefined> {
   const response = await fetch(`/api/userids`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username: username }),
+    body: JSON.stringify({ username: username.toLowerCase() }),
   });
 
   const res = await response.json();
