a54ad8eb02d5a33e27ecd18f5c678f5de5419e7c
midorinom <107839253+midorinom@users.noreply.github.com>
2024-12-01 18:06:44 +0800

    added isQueryingDatabase state

--- app/ui/settings/Settings.tsx
+++ app/ui/settings/Settings.tsx
@@ -7,6 +7,7 @@ import CreateAccount from "./CreateAccountButton";
 
 export default function Settings() {
   const [user, setUser] = useState<User | null>(null);
+  const [isQueryingDatabase, setIsQueryingDatabase] = useState<boolean>(false);
 
   useEffect(() => {
     const localUser = localStorage.getItem("user");
@@ -16,14 +17,20 @@ export default function Settings() {
   }, []);
 
   return (
-    <div className="relative mt-8 flex flex-col items-center gap-8">
-      {user &&
-        (user.username ? (
-          <SyncButton user={user} />
-        ) : (
-          <CreateAccount user={user} />
-        ))}
-      {user && <ResetButton user={user} />}
-    </div>
+    <>
+      {isQueryingDatabase ? (
+        <span className="loading loading-spinner m-auto h-1/3 w-auto text-accent"></span>
+      ) : (
+        <div className="relative mt-8 flex flex-col items-center gap-8">
+          {user &&
+            (user.username ? (
+              <SyncButton user={user} />
+            ) : (
+              <CreateAccount user={user} />
+            ))}
+          {user && <ResetButton user={user} />}
+        </div>
+      )}
+    </>
   );
 }
