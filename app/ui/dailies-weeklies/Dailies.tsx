fcef85ad9fca6845f6ffe8f9102bb56fbfbcb1eb
midorinom <107839253+midorinom@users.noreply.github.com>
2025-01-05 10:13:27 +0800

    added skeleton components for dailies and weeklies

---
+++ app/ui/dailies-weeklies/Dailies.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export default function Dailies() {
+  return (
+    <div className={`collapse collapse-open w-[32vw] gap-2 bg-primary pb-3`}>
+      <div className={`collapse-title mb-1 pb-0 pt-3`}>
+        <div className="flex gap-2">
+          <span className="text-4xl font-medium text-info underline-offset-8 underline-dreamy-neutral">
+            Dailies
+          </span>
+        </div>
+      </div>
+      <div className="absolute right-4 top-2 text-2xl text-info">test</div>
+    </div>
+  );
+}
