30a57211a5ba0bc5679b113fc1db56e3941aa5a2
midorinom <107839253+midorinom@users.noreply.github.com>
2025-01-06 16:55:29 +0800

    apply an initial filter of characters in parent component

--- app/ui/dailies-weeklies/DailiesWeeklies.tsx
+++ app/ui/dailies-weeklies/DailiesWeeklies.tsx
@@ -5,24 +5,39 @@ import Dailies from "./Dailies";
 import Weeklies from "./Weeklies";
 
 export default function DailiesWeeklies() {
-  const [characters, setCharacters] = useState<Character[]>([]);
+  const [dailiesCharacters, setDailiesCharacters] = useState<Character[]>([]);
+  const [weekliesCharacters, setWeekliesCharacters] = useState<Character[]>([]);
 
   useEffect(() => {
     const localUser = localStorage.getItem("user");
 
     if (localUser) {
       const parsedLocalUser: User = JSON.parse(localUser);
-      setCharacters(parsedLocalUser.characters);
+      const newDailiesCharacters: Character[] = [];
+      const newWeekliesCharacters: Character[] = [];
+
+      for (const character of parsedLocalUser.characters) {
+        if (character.dailies.length !== 0 && character.tracking.dailies) {
+          newDailiesCharacters.push(character);
+        }
+
+        if (character.weeklies.length !== 0 && character.tracking.weeklies) {
+          newWeekliesCharacters.push(character);
+        }
+      }
+
+      setDailiesCharacters(newDailiesCharacters);
+      setWeekliesCharacters(newWeekliesCharacters);
     }
   }, []);
 
   return (
     <main className="flex justify-around p-12">
-      {characters.length > 0 && (
-        <>
-          <Dailies charactersProp={characters} />
-          <Weeklies charactersProp={characters} />
-        </>
+      {dailiesCharacters.length > 0 && (
+        <Dailies charactersProp={dailiesCharacters} />
+      )}
+      {weekliesCharacters.length > 0 && (
+        <Weeklies charactersProp={weekliesCharacters} />
       )}
     </main>
   );
