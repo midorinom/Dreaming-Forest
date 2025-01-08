b54b6b5c547fddc7090b21bb6dc63b3b479b1940
midorinom <107839253+midorinom@users.noreply.github.com>
2025-01-08 22:41:23 +0800

    added skeletal UI for Checkboxes section

---
+++ app/ui/bosses/view/Checkboxes.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { CheckboxesProps } from "@/app/lib/definitions/bosses-definitions";
+
+export default function Checkboxes({}: CheckboxesProps) {
+  return (
+    <div className="collapse col-span-1 col-start-3 row-span-1 row-start-2 mx-auto h-full w-[96.5%] bg-primary/85 pr-16"></div>
+  );
+}
