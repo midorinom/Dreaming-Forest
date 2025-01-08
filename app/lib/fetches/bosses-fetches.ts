e7c82f11e62de009cdab7c923f6160e7204f06cb
midorinom <107839253+midorinom@users.noreply.github.com>
2025-01-08 14:42:28 +0800

    modified fetch function for bosses page to fetch all boss info

--- app/lib/fetches/bosses-fetches.ts
+++ app/lib/fetches/bosses-fetches.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { eq, min, and } from "drizzle-orm";
+import { asc } from "drizzle-orm";
 import { sql } from "@vercel/postgres";
 import { BossesInfo } from "@/drizzle/schema";
 import { unstable_noStore as noStore } from "next/cache";
@@ -10,32 +10,10 @@ export async function fetchBossesInfo(): Promise<FetchBossesInfoResponse> {
   noStore();
 
   try {
-    const minMesoSubquery = db
-      .select({
-        dashboard_position: BossesInfo.dashboard_position,
-        min_meso: min(BossesInfo.meso).as("min_meso"),
-      })
-      .from(BossesInfo)
-      .groupBy(BossesInfo.dashboard_position)
-      .as("minMesoSubquery");
-
-    const result = await db
+    const fetchedBossesInfo = await db
       .select()
       .from(BossesInfo)
-      .leftJoin(
-        minMesoSubquery,
-        and(
-          eq(BossesInfo.dashboard_position, minMesoSubquery.dashboard_position),
-          eq(BossesInfo.meso, minMesoSubquery.min_meso),
-        ),
-      );
-
-    // Flatten the result
-    const fetchedBossesInfo = result.map((row) => {
-      return {
-        ...row.bosses_info,
-      };
-    });
+      .orderBy(asc(BossesInfo.bosses_position));
 
     return fetchedBossesInfo;
   } catch (err) {
