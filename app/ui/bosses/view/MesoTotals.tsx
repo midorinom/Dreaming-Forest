eedbdbd7d5fb606d3e2749939bed2699ab764975
midorinom <107839253+midorinom@users.noreply.github.com>
2025-01-10 04:56:15 +0800

    display subtotals only if there is a character in that column

--- app/ui/bosses/view/MesoTotals.tsx
+++ app/ui/bosses/view/MesoTotals.tsx
@@ -3,7 +3,7 @@ import {
   MesoTotalsProps,
   Data,
 } from "@/app/lib/definitions/bosses-definitions";
-import { User } from "@/app/lib/definitions/general-definitions";
+import { Character, User } from "@/app/lib/definitions/general-definitions";
 import { getDateTimes } from "@/app/lib/functions/utility-functions";
 import _ from "lodash";
 
@@ -90,8 +90,15 @@ export default function MesoTotals({
 
       setData(newData);
       localStorage.setItem("user", JSON.stringify(newUser));
-      setCharacters(newUser.characters);
       setTotalMeso(newTotalMeso);
+
+      const newCharacters: Character[] = [];
+      for (const character of newUser.characters) {
+        if (character.bosses.length > 0 && character.tracking.bosses) {
+          newCharacters.push(character);
+        }
+      }
+      setCharacters(newCharacters);
     }
   }
 
@@ -105,38 +112,46 @@ export default function MesoTotals({
       >
         {data[charactersPage].subtotals[0].toLocaleString()}
       </div>
-      <div
-        className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
-        onClick={() => {
-          toggleByCharacter(1);
-        }}
-      >
-        {data[charactersPage].subtotals[1].toLocaleString()}
-      </div>
-      <div
-        className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
-        onClick={() => {
-          toggleByCharacter(2);
-        }}
-      >
-        {data[charactersPage].subtotals[2].toLocaleString()}
-      </div>
-      <div
-        className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
-        onClick={() => {
-          toggleByCharacter(3);
-        }}
-      >
-        {data[charactersPage].subtotals[3].toLocaleString()}
-      </div>
-      <div
-        className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
-        onClick={() => {
-          toggleByCharacter(4);
-        }}
-      >
-        {data[charactersPage].subtotals[4].toLocaleString()}
-      </div>
+      {data[charactersPage].subtotals[1] !== -1 && (
+        <div
+          className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
+          onClick={() => {
+            toggleByCharacter(1);
+          }}
+        >
+          {data[charactersPage].subtotals[1].toLocaleString()}
+        </div>
+      )}
+      {data[charactersPage].subtotals[2] !== -1 && (
+        <div
+          className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
+          onClick={() => {
+            toggleByCharacter(2);
+          }}
+        >
+          {data[charactersPage].subtotals[2].toLocaleString()}
+        </div>
+      )}
+      {data[charactersPage].subtotals[3] !== -1 && (
+        <div
+          className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
+          onClick={() => {
+            toggleByCharacter(3);
+          }}
+        >
+          {data[charactersPage].subtotals[3].toLocaleString()}
+        </div>
+      )}
+      {data[charactersPage].subtotals[4] !== -1 && (
+        <div
+          className="col-span-1 row-span-1 row-start-1 w-3/4 rounded-md bg-secondary p-1 text-center text-xl hover:cursor-pointer"
+          onClick={() => {
+            toggleByCharacter(4);
+          }}
+        >
+          {data[charactersPage].subtotals[4].toLocaleString()}
+        </div>
+      )}
       <div className="col-span-1 row-start-2 w-3/4 rounded-md bg-base-100 p-1 text-center text-xl">
         {totalMeso.toLocaleString()}
       </div>
